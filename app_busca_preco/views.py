4f754dfa2769b2d6b1e20525d1e4c2880ef3ab8d
ClaudioFritzen <sdfritzen96@gmail.com>
2023-03-06 19:57:59 +0000

    ajustamos para receber o template html

--- app_busca_preco/views.py
+++ app_busca_preco/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
 
 def pesquisar(request):
+    #return HttpResponse('<h1>Ola mundo!</h1> <br> <hr>')
     return render(request, 'produtos/pesquisa.html')
 
 
