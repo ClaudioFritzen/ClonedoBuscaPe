32dadd4ba21b1758aae19cd153773f9a0fae7d26
ClaudioFritzen <sdfritzen96@gmail.com>
2023-03-06 12:50:22 +0000

    criamos a models

---
+++ README.md
@@ -0,0 +1,8 @@
+# site para pegar imagens padrozado
+https://via.placeholder.com/150
+
+
+# utilizando o banco de dados Railway
+# cadastrar um cartão de credito como desenvolvedor, e terá até 5 dolar gratias. Só pagará se gastar mais de 5 dolares mensais
+
+# Criamos um novo projeto, Postgress.
--- app_busca_preco/models.py
+++ app_busca_preco/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    id_produto = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=300)
+    preco = models.DecimalField(decimal_places=2, max_digits=12)
+    site = models.TextField(error_messages=255)
+    data_cotacao = models.DateField()
+    link_imagem = models.TextField(max_length=500, default= 'https://via.placeholder.com/150')
+
+
+    def __str__(self):
+        return self.nome
+    
+    
